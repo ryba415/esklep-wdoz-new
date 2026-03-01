3edfeff8a7c27d7cbfbce2be3c5cfa3522494a13
unknown <darek@datum.pl>
2026-03-01 21:17:10 +0100

    zmiana napisu eSklep.wdoz.pl na wracamdozdrowia.pl

--- app/Http/Controllers/Pages/StaticPageController.php
+++ app/Http/Controllers/Pages/StaticPageController.php
@@ -19,101 +19,101 @@ class StaticPageController extends Controller
     {
 
     }
-    
 
-    
+
+
     public function mbankTest(Request $request){
         $viewData = [];
         $viewData['title'] = 'tu będzie płatność...';
         $viewData['content'] = 'tu będzie płatność za pomocą mbank<br><a href="/">wróć do strony głównej</a>';
-        
+
         return view('pages.static-page',$viewData);
     }
-    
+
     public function aptekaInternetowaPage(){
         $viewData = [];
         $viewData['title'] = 'Apteka internetowa';
         $viewData['content'] = '';
-        
+
         return view('pages.apteka-internetowa',$viewData);
     }
-    
+
     public function politykaPrywatnosciPage(){
         $viewData = [];
         $viewData['title'] = 'Polityka prywatności oraz mechanizm cookies Apteki Internetowej Wracam do zdrowia';
         $viewData['content'] = '';
         return view('pages/polityka-prywatnosci',$viewData);
     }
-    
+
     public function daneWojewódzkiegoInspektoratuPage(){
         $viewData = [];
         $viewData['title'] = 'Dane Wojewódzkiego Inspektoratu Farmaceutycznego';
         $viewData['content'] = '';
-        
+
         return view('pages.dane-wojewodzkiego-inspektoratu',$viewData);
     }
-    
+
     public function informacjePage(){
-        
+
         $viewData = [];
         $sectionData = [];
         $viewData['content'] = view('pages/informacje',$sectionData);
         $viewData['title'] = 'Informacje';
-        
+
         return view('pages.static-page',$viewData);
     }
-    
+
     public function regulaminPage(){
         $viewData = [];
         $viewData['title'] = 'REGULAMIN APTEKI INTERNETOWEJ WRACAM DO ZDROWIA';
         $viewData['content'] = '';
         return view('pages/regulamin-page',$viewData);
     }
-    
+
     public function kontaktPage(){
         $viewData = [];
         $sectionData = [];
         $viewData['content'] = view('pages/kontakt',$sectionData);
         $viewData['title'] = 'Kontakt';
-        
+
         return view('pages.static-page',$viewData);
     }
-    
+
     public function regulaminNewsletteraPage(){
         $viewData = [];
-        $viewData['title'] = 'Regulamin Newslettera Apteki Internetowej esklep.wdoz.pl';
+        $viewData['title'] = 'Regulamin Newslettera Apteki Internetowej wracamdozdrowia.pl';
         $viewData['content'] = '';
-        
+
         return view('pages.regulamin-newsletera',$viewData);
     }
-    
-    
+
+
     public function reklamacjezwrotyPage(){
-        
+
         $viewData = [];
         $sectionData = [];
         $viewData['content'] = view('pages/reklamacje-zwroty',$sectionData);
         $viewData['title'] = 'Reklamacje i zwroty';
-        
+
         return view('pages.static-page',$viewData);
     }
-    
+
     public function platnosciPage(){
         $viewData = [];
         $viewData['title'] = 'Płatności';
         $viewData['content'] = '';
-        
+
         return view('pages.platnosci',$viewData);
     }
-    
+
     public function kosztyDostawyPage(){
         $viewData = [];
         $sectionData = [];
         $viewData['content'] = view('pages/koszty-dostawy',$sectionData);
         $viewData['title'] = 'Koszty dostawy';
-        
+
         return view('pages.static-page',$viewData);
     }
-    
-    
+
+
 }
